c7bc14831062bf146ddafc9329d6b332d3e047af
Steve Quince <squince@glgroup.com>
2025-08-07 11:29:11 -0400

    now supports RSS feeds

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,14 +3,18 @@ import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
+	const posts = await getCollection('musings');
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
 		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.id}/`,
+			title: post.data.title,
+			description: post.data.description,
+			pubDate: post.data.pubDate,
+			updatedDate: post.data.updatedDate,
+			link: `/musings/${post.slug}/`,
 		})),
+		customData: `<language>en-us</language>`,
 	});
 }
